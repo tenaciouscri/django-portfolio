eb5587ebe2be80413f5b763e752461f727dd949f
tenaciouscri <cridelve@gmail.com>
2022-01-03 13:13:49 +0100

    Added models

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,9 @@
-# from django.shortcuts import render
+from django.shortcuts import render
+from projects.models import Project
 
-# Create your views here.
+def project_index(request, pk):
+    projects = Project.objects.all(pk=pk)
+    context = {
+        "projects": projects
+    }
+    return render(request, "project_index.html", context)
