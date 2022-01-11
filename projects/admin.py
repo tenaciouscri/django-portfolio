de615b6fe8d1c9ac8a944d202cdd1a7b77c1c135
tenaciouscri <cridelve@gmail.com>
2022-01-11 15:33:58 +0100

    Blog app templates

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,8 @@
-# from django.contrib import admin
+from django.contrib import admin
 
-# Register your models here.
+# from .models import Project
+
+# class ProjectAdmin(admin.ModelAdmin):
+#     pass
+
+# admin.site.register(Project, ProjectAdmin)
