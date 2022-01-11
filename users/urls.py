c5f3ee86b3764b9dc4d6cb05a0dca7083b37ce46
tenaciouscri <cridelve@gmail.com>
2022-01-11 16:17:47 +0100

    Added Django user management url

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from django.conf.urls import include
 from . import views
 
 urlpatterns = [
+    path("accounts/", include("django.contrib.auth.urls")),
     path("dashboard/", views.dashboard, name="dashboard"),
 ]
