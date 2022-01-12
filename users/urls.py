cc0b21f5cca668e337c1ab56af72ebffa5f53105
tenaciouscri <cridelve@gmail.com>
2022-01-12 11:35:20 +0100

    Created registration form for new non-admin users

--- users/urls.py
+++ users/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("accounts/", include("django.contrib.auth.urls")),
     path("dashboard/", views.dashboard, name="dashboard"),
+    path("register/", views.register, name="register"),
 ]
