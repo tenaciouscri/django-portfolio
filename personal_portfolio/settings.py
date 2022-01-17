e9823cac044473b48309d149cdc4019425791058
tenaciouscri <cridelve@gmail.com>
2022-01-17 08:56:48 +0100

    Removed secret key

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -20,8 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-=1ozm3fqi_5m#3(g#^2!5uw!j49obc=6-ys*3irw4^-)&ax^ki"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
